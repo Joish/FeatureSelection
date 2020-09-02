c16f1e5f117b6816274ad2863785b6afbd63bbe3
70i5h <joishj@dckap.com>
2020-09-02 17:13:35 +0530

    Added get_max_no_features()

--- ForwardFeatureSelection.py
+++ ForwardFeatureSelection.py
@@ -1,5 +1,18 @@
 
 class ForwardFeatureSelection:
-	def __init__(self,classifier):
-		self.classifier = classifier
-		
+    def __init__(self, classifier, X_train, y_train, X_test, y_test, min_no_features=0, max_no_features=None, log=False, verbose=1):
+        self.classifier = classifier
+        self.X_train = X_train
+        self.X_test = X_test
+        self.y_train = y_train
+        self.y_test = y_test
+        self.min_no_features = min_no_features
+        self.max_no_features = max_no_features
+        self.log = log
+        self.verbose = verbose
+
+        def get_max_no_features(self):
+            if self.max_no_features == None:
+                column_count = self.X_train.shape[1]
+                return column_count
+            return self.max_no_features
